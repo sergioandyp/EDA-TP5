befcb526d7d4bb328f7786279a2cd039ae5838c0
Juan Ignacio Causse <jcausse@itba.edu.ar>
2021-04-13 18:36:11 -0300

    Updated HTTPClient files

--- TP5-Client/HTTPClient.h
+++ TP5-Client/HTTPClient.h
@@ -1,8 +1,16 @@
 #ifndef HTTPCLIENT_H
 #define HTTPCLIENT_H
 
+//INCLUDES
 #include <iostream>
+#include <cstdlib>
+#include <string>
 #include <boost/asio.hpp>
 #include "cmdparser.h"
+
+//ERROR CODES
+#define MALLOC_ERROR -1
+#define PARSER_EXPECTED_ERROR -2
+#define PARSER_UNEXPECTED_ERROR -3
 
 #endif /*HTTPCLIENT_H*/
