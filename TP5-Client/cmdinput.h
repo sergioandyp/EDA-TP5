9e850187dba1389576485d9f1194871cc70c1d65
Juan Ignacio Causse <jcausse@itba.edu.ar>
2021-04-13 16:16:29 -0300

    Removed cmd input files

--- TP5-Client/cmdinput.h
+++
@@ -1,9 +0,0 @@
-#ifndef CMDINPUT_H
-#define CMDINPUT_H
-
-#include "cmdparser.h"
-#define PATH_MAX_LEN 150	//Maximum length (in chars) of the file path (including the null terminator) 
-
-void getPath(char*);		//Get path to HTML file
-
-#endif /*CMDINPUT_H*/
