31531f64ac842601f9253492fc1e99b999e6f6b8
Juan Ignacio Causse <jcausse@itba.edu.ar>
2021-04-15 13:06:45 -0300

    Merged ClientBase.cpp and ClientBase.h with HTTPClient.cpp and HTTPClient.h. Client is now ready to test

--- TP5-Client/HTTPClient.cpp
+++ TP5-Client/HTTPClient.cpp
@@ -5,12 +5,22 @@
 
 #include "HTTPClient.h"
 
+#pragma warning(disable : 4996)
+
 #define PATH_MAX_LEN 150	//Maximum length (in chars) of the file path (including the null terminator)
 
+//FUNCTION PROTOTYPES
+int client(int argc, char* argv[]);
 int getFilePathCallback (char* key, char* value, void* userData);
 
+int main (int argc, char ** argv){
+    client(argc, argv); //Run client
+    return 0;
+}
+
+//MAIN HTTP CLIENT FUNCTION
 int client(int argc, char* argv[]) {
-	//GET FILE PATH FROM COMMAND LINE
+	//GET FILE PATH FROM COMMAND LINE USING PARSER
 	char* filePath = (char*)malloc(PATH_MAX_LEN * sizeof(char));
 	if (filePath == NULL)
 		return MALLOC_ERROR;
@@ -38,6 +48,7 @@ int client(int argc, char* argv[]) {
 	return EXIT_SUCCESS;
 }
 
+//CALLBACK FOR PARSER
 int getFilePathCallback(char* key, char* value, void* userData) {
 	if (key != NULL)
 		return 0;
@@ -47,9 +58,55 @@ int getFilePathCallback(char* key, char* value, void* userData) {
 	return 1;
 }
 
-int main (int argc, char ** argv){
-    client(argc, argv);
-    return 0;
+//HTTP CLIENT CLASS METHOD DEFINITIONS
+
+//CLASS CONSTRUCTOR
+HTTPClient::HTTPClient(std::string requestURL_) : requestURL(requestURL_) {
 }
 
-
+//CONNECTION METHOD FOR THE HTTP CLIENT
+int HTTPClient::connect() {
+    std::string filename = requestURL.substr(requestURL.rfind("/")+1);	 // Nombre del arhivo a partir de la ultima barra
+    if (filename.empty() || filename == requestURL) {
+        std::cout << "ERROR: filename not specified" << std::endl;
+        return 1;
+    }
+    std::cout << "Looking for file: " << filename << std::endl;
+    FILE* file = fopen(filename.c_str(), "w");
+    if (file) {
+        CURLcode error = curl_global_init(CURL_GLOBAL_ALL);
+        if (error == CURLE_OK) {
+            CURL* curl = curl_easy_init();
+            if (curl) {
+                //CURL SETUP
+                curl_easy_setopt(curl, CURLOPT_URL, requestURL.c_str());
+                curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
+                curl_easy_setopt(curl, CURLOPT_PORT, 80);
+                //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+                curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);	// Que falle en caso de 404 o similar
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);	// Por default, guarda la data en un archivo
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);	// le paso el puntero al archivo
+                //CURL PERFORM CONNECTION
+                CURLcode code = curl_easy_perform(curl);
+                if (code == CURLE_OK) {
+                    std::cout << "Operation succeded" << std::endl;
+                }
+                else if (code == CURLE_HTTP_RETURNED_ERROR) {
+                    std::cout << "ERROR: the requested file was not found in the server" << std::endl;
+                }
+                else {
+                    std::cout << "ERROR: an unexpected error ocurred" << std::endl;
+                }
+            }
+            //CLEANUP CURL MEMORY AFTER USE
+            curl_easy_cleanup(curl);
+        }
+        //GENERAL CLEANUP
+        fclose(file);
+        curl_global_cleanup();
+    }
+    else {
+        std::cout << "ERROR: cannot open the filename specified" << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
--- TP5-Client/HTTPClient.h
+++ TP5-Client/HTTPClient.h
@@ -5,8 +5,20 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <fstream>
 #include <curl/curl.h>
 #include "cmdparser.h"
+
+//CLASSES
+class HTTPClient {
+public:
+    HTTPClient(std::string requestURL_);    //Constructor
+    int connect();                          //Method used to connect to server
+
+private:
+    std::string requestURL;                 //URL path to request
+    std::ifstream file;                     //File to save server response
+};
 
 //ERROR CODES
 #define MALLOC_ERROR -1
