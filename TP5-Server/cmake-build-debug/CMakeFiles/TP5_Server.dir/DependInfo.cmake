76395304c5c750262006564f334e70a8571ac764
Juan Ignacio Causse <jcausse@itba.edu.ar>
2021-04-15 22:00:45 -0300

    Last commit

---
+++ TP5-Server/cmake-build-debug/CMakeFiles/TP5_Server.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/juani/Desktop/EDA-TP5/TP5-Server/HTTPServer.cpp" "/Users/juani/Desktop/EDA-TP5/TP5-Server/cmake-build-debug/CMakeFiles/TP5_Server.dir/HTTPServer.cpp.o"
+  "/Users/juani/Desktop/EDA-TP5/TP5-Server/main.cpp" "/Users/juani/Desktop/EDA-TP5/TP5-Server/cmake-build-debug/CMakeFiles/TP5_Server.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TP5-Server/cmake-build-debug/CMakeFiles/TP5_Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP5_Server.dir/HTTPServer.cpp.o"
+  "CMakeFiles/TP5_Server.dir/main.cpp.o"
+  "TP5_Server"
+  "TP5_Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP5_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
